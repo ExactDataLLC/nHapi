9086a55d0f5bcf5647aab8f3da5b85b51e48120e
Douglass Huang <douglass.huang@adillc.net>
2016-05-11 09:38:17 -0400

    Added test for parsing RDE_O11 with AL1.

--- NHapi20/NHapi.NUnit/PipeParsingFixture251.cs
+++ NHapi20/NHapi.NUnit/PipeParsingFixture251.cs
@@ -94,5 +94,27 @@ OBX|3|TM|||TMValue||||||F"
 			isSame = string.Compare("ADT_A01", result, StringComparison.InvariantCultureIgnoreCase) == 0;
 			Assert.IsTrue(isSame, "ADT_A01 returns ADT_A01");
 		}
+
+	    [Test]
+	    public void Parse_RdeO11WithAL1_AL1IsInPatientGroup()
+	    {
+	        // setup
+            PipeParser parser = new PipeParser();
+	        string encodedRdeO11 =
+	            @"MSH|^~\&|EXACTDATA||ALL|ALL|20101217181500||RDE^O11^RDE_O11|EF010000000071000000_4|T^T|2.5.1
+PID|1||7010566270^^^^DODID~EM010000008000000^^^^MR||ZZEDConner^Dean^J^^Mr.||19810611|MALE||WHITE^^HL70005|^^^^^USA^H||||ENGLISH^^HL70296|SINGLE^^HL70002||EF010000000071000000||||NOTHISPANICORLATINO^^HL70189|||||ACTIVEDUTY^^HL70172
+PV1|1|OUTPATIENT|^^^MBCC^^AMBLOC||||1853210951^Pennington^Zachary|||MEDICINEGENERAL|||||||1853210951^Pennington^Zachary|||||||||||||||||||01|||||ACTIVE|||20101217181500|20101217184500||||||V
+PV2|||V70.0^General medical examination^I9C|||||||||||||||||||||||||||||||||||||||||N
+AL1|1|DRUG^^HL70127|70618^Penicillin^RXNORM|MODERATE^^HL70128|Dry Mouth|20100928
+ORC|NW|101217-833651^ExD|||ORDERED
+RXE|^^^20101217184500|54569-4888-0^Oseltamivir|1||75 mg||^po bid x 5d||N|10||0||||||||||||||||||||20101217184500
+RXR|^Oral^HL70162".Replace('\n', '\r');
+
+	        // method under test
+	        RDE_O11 rdeO11 = parser.Parse(encodedRdeO11) as RDE_O11;
+
+	        // assertions
+            Assert.That(rdeO11.PATIENT.AL1RepetitionsUsed, Is.EqualTo(1));
+	    }
 	}
 }
